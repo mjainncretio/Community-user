7b1147f734aa874fe30229c16256ce3714a8c9a7
mjainncretio <120638988+mjainncretio@users.noreply.github.com>
2022-12-23 17:18:09 +0530

    Update createCustomUserButton.js

--- Create Community User/force-app/main/default/lwc/createCustomUserButton/createCustomUserButton.js
+++ Create Community User/force-app/main/default/lwc/createCustomUserButton/createCustomUserButton.js
@@ -32,28 +32,11 @@ export default class CreateCustomUserButton extends LightningElement
    
         .catch(error =>
             {
-                //toast messages when error in creation of user
-               if(error.body.message === 'Insert failed. First exception on row 0; first error: UNKNOWN_EXCEPTION, portal account owner must have a role: []')
-               {
-                this.showToast('ERROR!', 'Contact does not have a account associated to it !!', 'Error');
-               }
-               else if(error.body.message === 'Insert failed. First exception on row 0; first error: LICENSE_LIMIT_EXCEEDED, License Limit Exceeded - Customer Community: []')
-               {
-                this.showToast('ERROR!', 'License Limit Exceeded', 'Error');
-               }
-               else if(error.body.message === 'Insert failed. First exception on row 0; first error: PORTAL_USER_ALREADY_EXISTS_FOR_CONTACT, portal user already exists for contact: []')
-               {
-                this.showToast('ERROR!', 'portal user already exists for contact!!', 'Error');
-               }
-               else if(error.body.message === 'Insert failed. First exception on row 0; first error: FIELD_INTEGRITY_EXCEPTION, Username must be in the form of an email address (for example, john@acme.com): Username: [Username]')
-               {
-                this.showToast('ERROR!', 'contact does not have a email!!', 'Error');
-               }
-               else
-               {
-                this.showToast('ERROR!', error.body.message, 'Error');
-               }
-        });
+                var errorMessageBody = error.body.message;
+                var errorMessage = errorMessageBody.substring(errorMessageBody.indexOf(":") + 1, 
+                                                              errorMessageBody.lastIndexOf(":"));
+                this.showToast('ERROR!', errorMessage, 'Error');            
+            });
     }
 
 }
